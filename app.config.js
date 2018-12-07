10bd9943424731fe01424e63644b8eefec465ae7
Michal Nedzka <Michal.Nedzka@stepstone.com>
2018-12-07 11:06:17 +0100

    Sprint 61 presentation proposition

--- app.config.js
+++ app.config.js
@@ -21,16 +21,21 @@ module.exports = {
             theme: 'ocean',
         },
         {
-          file: 'ocean-team-sprint60.html',
-          title: 'Ocean Team - Sprint 60 review',
-          theme: 'ocean',
+            file: 'ocean-team-sprint60.html',
+            title: 'Ocean Team - Sprint 60 review',
+            theme: 'ocean',
+        },
+        {
+            file: 'ocean-team-sprint61.html',
+            title: 'Ocean Team - Sprint 61 review',
+            theme: 'ocean',
         },
     ],
     reveal: {
         dependencies: [
-            {src: 'plugin/markdown/marked.js'},
-            {src: 'plugin/markdown/markdown.js'},
-            {src: 'plugin/notes/notes.js', async: true},
+            { src: 'plugin/markdown/marked.js' },
+            { src: 'plugin/markdown/markdown.js' },
+            { src: 'plugin/notes/notes.js', async: true },
             {
                 src: 'plugin/highlight/highlight.js', async: true, callback: function () {
                     hljs.initHighlightingOnLoad();
