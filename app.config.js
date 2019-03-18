731d4a221851c74015eb46d32ab818b56f2fa2e8
Michal Nedzka <Michal.Nedzka@stepstone.com>
2019-03-18 07:30:03 +0100

    Presentation draft

--- app.config.js
+++ app.config.js
@@ -11,21 +11,6 @@ module.exports = {
             theme: 'ocean',
         },
         {
-            file: 'ocean-team-sprint62.html',
-            title: 'Ocean Team - Sprint 62 review',
-            theme: 'ocean',
-        },
-        {
-            file: 'ocean-team-sprint63.html',
-            title: 'Ocean Team - Sprint 63 review',
-            theme: 'ocean',
-        },
-        {
-            file: 'ocean-team-sprint64.html',
-            title: 'Ocean Team - Sprint 64 review',
-            theme: 'ocean',
-        },
-        {
             file: 'ocean-team-sprint65.html',
             title: 'Ocean Team - Sprint 65 review',
             theme: 'ocean',
@@ -33,6 +18,11 @@ module.exports = {
         {
             file: 'ocean-team-sprint66.html',
             title: 'Ocean Team - Sprint 66 review',
+            theme: 'ocean',
+        },
+        {
+            file: 'ocean-team-sprint67.html',
+            title: 'Ocean Team - Sprint 67 review',
             theme: 'ocean',
         },
     ],
